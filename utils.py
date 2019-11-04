248d3a42bd807b78ea5a91e43547893c462c8b0e
Борис Цейтлин <euphetar@gmail.com>
2019-11-04 15:56:27 +0300

    initial

---
+++ utils.py
@@ -0,0 +1,205 @@
+import pandas as pd
+import numpy as np
+from string import punctuation
+import datetime
+import re
+
+def clean_StadiumType(txt):
+    if pd.isna(txt):
+        return np.nan
+    txt = txt.lower()
+    txt = ''.join([c for c in txt if c not in punctuation])
+    txt = re.sub(' +', ' ', txt)
+    txt = txt.strip()
+    txt = txt.replace('outside', 'outdoor')
+    txt = txt.replace('outdor', 'outdoor')
+    txt = txt.replace('outddors', 'outdoor')
+    txt = txt.replace('outdoors', 'outdoor')
+    txt = txt.replace('oudoor', 'outdoor')
+    txt = txt.replace('indoors', 'indoor')
+    txt = txt.replace('ourdoor', 'outdoor')
+    txt = txt.replace('retractable', 'rtr.')
+    return txt
+
+def transform_StadiumType(txt):
+    if pd.isna(txt):
+        return np.nan
+    if 'outdoor' in txt or 'open' in txt:
+        return 1
+    if 'indoor' in txt or 'closed' in txt:
+        return 0
+    return np.nan
+
+def str_to_seconds(txt):
+    txt = txt.split(':')
+    ans = int(txt[0])*60 + int(txt[1]) + int(txt[2])/60
+    return ans
+
+def str_to_float(txt):
+    try:
+        return float(txt)
+    except Exception as e:
+        return np.NaN
+
+def map_weather(txt):
+    ans = 1
+    if pd.isna(txt):
+        return 0
+    if 'partly' in txt:
+        ans*=0.5
+    if 'climate controlled' in txt or 'indoor' in txt:
+        return ans*3
+    if 'sunny' in txt or 'sun' in txt:
+        return ans*2
+    if 'clear' in txt:
+        return ans
+    if 'cloudy' in txt:
+        return -ans
+    if 'rain' in txt or 'rainy' in txt:
+        return -2*ans
+    if 'snow' in txt:
+        return -3*ans
+    return 0
+
+def new_orientation(angle, play_direction):
+    if play_direction == 0:
+        new_angle = 360.0 - angle
+        if new_angle == 360.0:
+            new_angle = 0.0
+        return new_angle
+    else:
+        return angle
+
+def preprocess_features(df):
+    """Accepts df like train data, returns X, y"""
+
+    # Feature engineering
+    df['DefendersInTheBox_vs_Distance'] = (df['DefendersInTheBox'] / df['Distance'])
+    df['StadiumType'] = df['StadiumType'].apply(clean_StadiumType)
+    df['StadiumTypeShort'] = df['StadiumType'].apply(transform_StadiumType)
+    df = df.drop(['StadiumType'], axis=1)
+
+    Turf = {'Field Turf':'Artificial', 'A-Turf Titan':'Artificial', 'Grass':'Natural', 'UBU Sports Speed S5-M':'Artificial', 
+        'Artificial':'Artificial', 'DD GrassMaster':'Artificial', 'Natural Grass':'Natural', 
+        'UBU Speed Series-S5-M':'Artificial', 'FieldTurf':'Artificial', 'FieldTurf 360':'Artificial', 'Natural grass':'Natural', 'grass':'Natural', 
+        'Natural':'Natural', 'Artifical':'Artificial', 'FieldTurf360':'Artificial', 'Naturall Grass':'Natural', 'Field turf':'Artificial', 
+        'SISGrass':'Artificial', 'Twenty-Four/Seven Turf':'Artificial', 'natural grass':'Natural'} 
+
+    turf_type = df['Turf'].map(Turf)
+    df['TurfIsNatural'] = (turf_type == 'Natural')
+    df = df.drop(['Turf'], axis=1)
+
+    map_abbr = {'ARI': 'ARZ', 'BAL': 'BLT', 'CLE': 'CLV', 'HOU': 'HST'}
+    for abb in df['PossessionTeam'].unique():
+        map_abbr[abb] = abb
+    df['PossessionTeam'] = df['PossessionTeam'].map(map_abbr)
+    df['HomeTeamAbbr'] = df['HomeTeamAbbr'].map(map_abbr)
+    df['VisitorTeamAbbr'] = df['VisitorTeamAbbr'].map(map_abbr)
+
+    df['HomePossesion'] = (df['PossessionTeam'] == df['HomeTeamAbbr'])
+
+    df['Field_eq_Possession'] = df['FieldPosition'] == df['PossessionTeam']
+    df['HomeField'] = df['FieldPosition'] == df['HomeTeamAbbr']
+
+    df = pd.concat([df.drop(['OffenseFormation'], axis=1), pd.get_dummies(df['OffenseFormation'], prefix='Formation')], axis=1)
+    
+    df['GameClock'] = df['GameClock'].apply(str_to_seconds)
+
+    df['PlayerHeight'] = df['PlayerHeight'].apply(lambda x: 12*int(x.split('-')[0])+int(x.split('-')[1]))
+    df['PlayerBMI'] = 703*(df['PlayerWeight']/(df['PlayerHeight'])**2)
+
+    df['TimeHandoff'] = pd.to_datetime(df['TimeHandoff'], utc=True)
+    df['TimeSnap'] = pd.to_datetime(df['TimeSnap'], utc=True)
+    df['TimeDelta'] = df.apply(lambda row: (row['TimeHandoff'] - row['TimeSnap']).total_seconds(), axis=1)
+    df['PlayerBirthDate'] = df['PlayerBirthDate'].apply(lambda x: datetime.datetime.strptime(x, "%m/%d/%Y"))
+    df['PlayerBirthDate'] = pd.to_datetime(df['PlayerBirthDate'], utc=True)
+
+    seconds_in_year = 60*60*24*365.25
+    df['PlayerAge'] = df.apply(lambda row: (row['TimeHandoff']-row['PlayerBirthDate']).total_seconds()/seconds_in_year, axis=1)
+    df = df.drop(['TimeHandoff', 'TimeSnap', 'PlayerBirthDate'], axis=1)
+
+    df['WindSpeed'] = df['WindSpeed'].apply(lambda x: x.lower().replace('mph', '').strip() if not pd.isna(x) else x)
+    df['WindSpeed'] = df['WindSpeed'].apply(lambda x: (int(x.split('-')[0])+int(x.split('-')[1]))/2 if not pd.isna(x) and '-' in x else x)
+    df['WindSpeed'] = df['WindSpeed'].apply(lambda x: (int(x.split()[0])+int(x.split()[-1]))/2 if not pd.isna(x) and type(x)!=float and 'gusts up to' in x else x)
+    df['WindSpeed'] = df['WindSpeed'].apply(str_to_float)
+    df['WindSpeed'] = df['WindSpeed'].fillna(method='pad')
+
+    df = df.drop(['WindDirection'], axis=1)
+    df['PlayDirection'] = df['PlayDirection'].apply(lambda x: x.strip() == 'right')
+    df['Team'] = df['Team'].apply(lambda x: x.strip()=='home')
+
+    df['GameWeather'] = df['GameWeather'].str.lower()
+    indoor = "indoor"
+    df['GameWeather'] = df['GameWeather'].apply(lambda x: indoor if not pd.isna(x) and indoor in x else x)
+    df['GameWeather'] = df['GameWeather'].apply(lambda x: x.replace('coudy', 'cloudy').replace('clouidy', 'cloudy').replace('party', 'partly') if not pd.isna(x) else x)
+    df['GameWeather'] = df['GameWeather'].apply(lambda x: x.replace('clear and sunny', 'sunny and clear') if not pd.isna(x) else x)
+    df['GameWeather'] = df['GameWeather'].apply(lambda x: x.replace('skies', '').replace("mostly", "").strip() if not pd.isna(x) else x)
+    df['GameWeather'] = df['GameWeather'].apply(map_weather)
+
+    df['IsRusher'] = df['NflId'] == df['NflIdRusher']
+    df.drop(['NflId', 'NflIdRusher'], axis=1, inplace=True)
+
+    df['X'] = df.apply(lambda row: row['X'] if row['PlayDirection'] else 120-row['X'], axis=1)
+    df['Orientation'] = df.apply(lambda row: new_orientation(row['Orientation'], row['PlayDirection']), axis=1)
+    df['Dir'] = df.apply(lambda row: new_orientation(row['Dir'], row['PlayDirection']), axis=1)
+    
+    df['YardsLeft'] = df.apply(lambda row: 100-row['YardLine'] if row['HomeField'] else row['YardLine'], axis=1)
+    df['YardsLeft'] = df.apply(lambda row: row['YardsLeft'] if row['PlayDirection'] else 100-row['YardsLeft'], axis=1)
+    df.drop(df.index[(df['YardsLeft']<df['Yards']) | (df['YardsLeft']-100>df['Yards'])], inplace=True)
+    return df
+
+def sort_df(df):
+    df = df.sort_values(by=['PlayId', 'Team', 'IsRusher', 'JerseyNumber']).reset_index(drop=True)
+    return df
+
+def make_x(df):
+    df = sort_df(df)
+
+    cols_delete = ['GameId', 'PlayId', 'IsRusher', 'Team']
+    df = df.drop(cols_delete, axis=1)
+
+    # Fill nan
+    df = df.fillna(method='pad')
+
+    # Text features
+    text = []
+    for col in df.columns:
+        if df[col].dtype =='object':
+            text.append(col)
+
+    df = df.drop(text, axis=1)
+
+    # Player features
+    cols_player = ['X',
+         'Y',
+         'S',
+         'A',
+         'Dis',
+         'Orientation',
+         'Dir',
+         'JerseyNumber',
+         'PlayerHeight',
+         'PlayerWeight',
+         'PlayerBMI',
+         'PlayerAge']
+    X = np.array(df[cols_player]).reshape(-1, len(cols_player)*22)
+    assert df[cols_player].shape[0] == X.shape[0] * 22
+    assert df[cols_player].shape[1] == X.shape[1] / 22
+
+    # Play features
+    cols_play = list(df.drop(cols_player+(['Yards'] if 'Yards' in df.columns else []), axis=1).columns)
+    X_play_col = np.zeros(shape=(X.shape[0], len(cols_play)))
+    for i, col in enumerate(cols_play):
+        X_play_col[:, i] = df[col][::22]
+
+    assert X.shape[0] == X_play_col.shape[0]
+    X = np.concatenate([X, X_play_col], axis=1)
+
+    return X
+
+def make_y(X, df):
+    y = np.zeros(shape=(X.shape[0], 199))
+    for i, yard in enumerate(df['Yards'][::22]):
+        y[i, yard+99:] = np.ones(shape=(1, 100-yard))
+    return y
+
