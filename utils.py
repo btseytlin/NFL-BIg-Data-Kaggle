5a2323eb57137604401b85485e48cff1a974772c
Борис Цейтлин <euphetar@gmail.com>
2019-11-13 20:32:20 +0300

    Add orientation fix, clean up code

--- utils.py
+++ utils.py
@@ -4,6 +4,7 @@ from string import punctuation
 import datetime
 import re
 import torch
+import math
 
 def crps(y_true, y_pred):
     return np.mean(np.square(y_true - y_pred), axis=1)
@@ -96,24 +97,79 @@ def map_weather(txt):
         return -3*ans
     return 0
 
-def new_orientation(angle, play_direction):
-    if play_direction == 0:
-        new_angle = 360.0 - angle
-        if new_angle == 360.0:
-            new_angle = 0.0
-        return new_angle
-    else:
-        return angle
+def final_drop_cols(df):
+    """
+        Drop columns not used in X
+    """
+    cols_to_drop = [
+        'Yards',
+        'PlayDirection', 
+        'TeamOnOffense', 'NflId', 'NflIdRusher',
+        'TimeHandoff', 'TimeSnap', 'PlayerBirthDate',
+        'FieldPosition',
+         'DisplayName',
+         'PossessionTeam',
+         'PlayerCollegeName',
+         'Position',
+         'HomeTeamAbbr',
+         'VisitorTeamAbbr',
+         'Stadium',
+         'Location',
+         'GameId', 'PlayId', 'IsRusher', 'Team',
+         'IsRusher']
+    cols_to_drop = list(set(cols_to_drop).intersection(set(list(df.columns))))
+    df = df.drop(cols_to_drop, axis=1)
+    return df
 
-def preprocess_features(df, fillna=False):
-    """Accepts df like train data, returns X, y"""
+def standartize_orientations(df):
+    """
+        https://www.kaggle.com/cpmpml/initial-wrangling-voronoi-areas-in-python
+    
+        Make sure the offensive team is always moving left to right.
+    """
+    df['ToLeft'] = df.PlayDirection == "left"
+    df['TeamOnOffense'] = "home"
+    df.loc[df.PossessionTeam != df.HomeTeamAbbr, 'TeamOnOffense'] = "away"
+    df['IsOnOffense'] = df.Team == df.TeamOnOffense # Is player on offense?
+    df['HomeOnOffense'] = (df['TeamOnOffense'] == 'home').astype(int)
 
-    # Feature engineering
-    df['DefendersInTheBox_vs_Distance'] = (df['DefendersInTheBox'] / df['Distance'])
-    df['StadiumType'] = df['StadiumType'].apply(clean_StadiumType)
-    df['StadiumTypeShort'] = df['StadiumType'].apply(transform_StadiumType)
-    df = df.drop(['StadiumType'], axis=1)
+    df['YardLine_std'] = 100 - df.YardLine
+    df.loc[df.FieldPosition.fillna('') == df.PossessionTeam,  
+              'YardLine_std'
+             ] = df.loc[df.FieldPosition.fillna('') == df.PossessionTeam,  
+              'YardLine']
+    df['X_std'] = df.X
+    df.loc[df.ToLeft, 'X_std'] = 120 - df.loc[df.ToLeft, 'X'] 
+    df['Y_std'] = df.Y
+    df.loc[df.ToLeft, 'Y_std'] = 160/3 - df.loc[df.ToLeft, 'Y'] 
+    
+    df['Dir_rad'] = np.mod(90 - df.Dir, 360) * math.pi/180.0
+    df['Dir_std'] = df.Dir_rad
+    df.loc[df.ToLeft, 'Dir_std'] = np.mod(np.pi + df.loc[df.ToLeft, 'Dir_rad'], 2*np.pi)
 
+    df['Orientation_rad'] = np.mod(df.Orientation, 360) * math.pi/180.0
+
+    df.loc[df.Season >= 2018, 'Orientation_rad'
+         ] = np.mod(df.loc[df.Season >= 2018, 'Orientation'] - 90, 360) * math.pi/180.0
+
+    df['Orientation_rad'] = np.mod(df.Orientation, 360) * math.pi/180.0
+    df.loc[df.Season >= 2018, 'Orientation_rad'
+             ] = np.mod(df.loc[df.Season >= 2018, 'Orientation'] - 90, 360) * math.pi/180.0
+    df['Orientation_std'] = df.Orientation_rad
+    df.loc[df.ToLeft, 'Orientation_std'] = np.mod(math.pi + df.loc[df.ToLeft, 'Orientation_rad'], 2*math.pi)
+    
+    replace_cols = ['YardLine', 'X', 'Y', 'Dir', 'Orientation']
+    for col in replace_cols:
+        df[col] = df[col+'_std']
+        df.drop([col+'_std'], axis=1, inplace=True)
+
+    drop_cols = ['Dir_rad', 'Orientation_rad']
+    for col in drop_cols:
+        df.drop([col], axis=1, inplace=True)
+
+    return df
+
+def clean_turf(df):
     Turf = {'Field Turf':'Artificial', 'A-Turf Titan':'Artificial', 'Grass':'Natural', 'UBU Sports Speed S5-M':'Artificial', 
         'Artificial':'Artificial', 'DD GrassMaster':'Artificial', 'Natural Grass':'Natural', 
         'UBU Speed Series-S5-M':'Artificial', 'FieldTurf':'Artificial', 'FieldTurf 360':'Artificial', 'Natural grass':'Natural', 'grass':'Natural', 
@@ -123,7 +179,9 @@ def preprocess_features(df, fillna=False):
     turf_type = df['Turf'].map(Turf)
     df['TurfIsNatural'] = (turf_type == 'Natural')
     df = df.drop(['Turf'], axis=1)
+    return df
 
+def clean_abbrs(df):
     # CAREFUL. What if a new team appears?
     map_abbr = {'ARI': 'ARZ', 'BAL': 'BLT', 'CLE': 'CLV', 'HOU': 'HST'}
     for abb in df['PossessionTeam'].unique():
@@ -138,15 +196,9 @@ def preprocess_features(df, fillna=False):
     df['PossessionTeam'] = df['PossessionTeam'].apply(safe_map)
     df['HomeTeamAbbr'] = df['HomeTeamAbbr'].apply(safe_map)
     df['VisitorTeamAbbr'] = df['VisitorTeamAbbr'].apply(safe_map)
+    return df
 
-    df['HomePossesion'] = (df['PossessionTeam'] == df['HomeTeamAbbr'])
-
-    df['Field_eq_Possession'] = df['FieldPosition'] == df['PossessionTeam']
-    df['HomeField'] = df['FieldPosition'] == df['HomeTeamAbbr']
-
-    # in posession
-    df['InPosession']=(((df.Team == 'home') & (df.PossessionTeam == df.HomeTeamAbbr)) | ((df.Team == 'away') & (df.PossessionTeam == df.VisitorTeamAbbr)))
-
+def encode_formations(df):
     # Formation columns
     df = pd.concat([df.drop(['OffenseFormation'], axis=1), pd.get_dummies(df['OffenseFormation'], prefix='OffenseFormation')], axis=1)
     # Filling missing dummy columns at test stage
@@ -161,33 +213,15 @@ def preprocess_features(df, fillna=False):
     for col in expected_columns:
         if not col in df.columns:
             df[col] = 0
+    return df 
 
-
-    df['GameClock'] = df['GameClock'].apply(str_to_seconds)
-
-    df['PlayerHeight'] = df['PlayerHeight'].apply(lambda x: 12*int(x.split('-')[0])+int(x.split('-')[1]))
-    df['PlayerBMI'] = 703*(df['PlayerWeight']/(df['PlayerHeight'])**2)
-
-    df['TimeHandoff'] = pd.to_datetime(df['TimeHandoff'], utc=True)
-    df['TimeSnap'] = pd.to_datetime(df['TimeSnap'], utc=True)
-    df['TimeDelta'] = (df['TimeHandoff']-df['TimeSnap']).apply(lambda x: x.total_seconds())
-    df['PlayerBirthDate'] = df['PlayerBirthDate'].apply(lambda x: datetime.datetime.strptime(x, "%m/%d/%Y"))
-    df['PlayerBirthDate'] = pd.to_datetime(df['PlayerBirthDate'], utc=True)
-
-    seconds_in_year = 60*60*24*365.25
-    df['PlayerAge'] = (df['TimeHandoff']-df['PlayerBirthDate']).apply(lambda x: x.total_seconds())/seconds_in_year
-    df = df.drop(['TimeHandoff', 'TimeSnap', 'PlayerBirthDate'], axis=1)
-
+def clean_weather(df):
     df['WindSpeed'] = df['WindSpeed'].apply(lambda x: x.lower().replace('mph', '').strip() if not pd.isna(x) else x)
     df['WindSpeed'] = df['WindSpeed'].apply(lambda x: (int(x.split('-')[0])+int(x.split('-')[1]))/2 if not pd.isna(x) and '-' in x else x)
     df['WindSpeed'] = df['WindSpeed'].apply(lambda x: (int(x.split()[0])+int(x.split()[-1]))/2 if not pd.isna(x) and type(x)!=float and 'gusts up to' in x else x)
     df['WindSpeed'] = df['WindSpeed'].apply(str_to_float)
 
     df = df.drop(['WindDirection'], axis=1)
-    df['PlayDirection'] = df['PlayDirection'].apply(lambda x: x.strip() == 'right')
-    df['IsHomeTeam'] = df['Team'].apply(lambda x: x.strip()=='home')
-
-
 
     df['GameWeather'] = df['GameWeather'].str.lower()
     indoor = "indoor"
@@ -196,18 +230,9 @@ def preprocess_features(df, fillna=False):
     df['GameWeather'] = df['GameWeather'].apply(lambda x: x.replace('clear and sunny', 'sunny and clear') if not pd.isna(x) else x)
     df['GameWeather'] = df['GameWeather'].apply(lambda x: x.replace('skies', '').replace("mostly", "").strip() if not pd.isna(x) else x)
     df['GameWeather'] = df['GameWeather'].apply(map_weather)
+    return df
 
-    df['IsRusher'] = df['NflId'] == df['NflIdRusher']
-    df.drop(['NflId', 'NflIdRusher'], axis=1, inplace=True)
-
-    df['X'] = df.apply(lambda row: row['X'] if row['PlayDirection'] else 120-row['X'], axis=1)
-    df['Orientation'] = df.apply(lambda row: new_orientation(row['Orientation'], row['PlayDirection']), axis=1)
-    df['Dir'] = df.apply(lambda row: new_orientation(row['Dir'], row['PlayDirection']), axis=1)
-    
-    df['YardsLeft'] = df.apply(lambda row: 100-row['YardLine'] if row['HomeField'] else row['YardLine'], axis=1)
-    df['YardsLeft'] = df.apply(lambda row: row['YardsLeft'] if row['PlayDirection'] else 100-row['YardsLeft'], axis=1)
-    
-
+def encode_personell(df):
     # DefensePersonnel
     counts = []
     for i, val in df['DefensePersonnel'].str.split(',').iteritems():
@@ -242,38 +267,73 @@ def preprocess_features(df, fillna=False):
     offense_personnel_df = offense_personnel_df.fillna(0).astype(int)
     offense_personnel_df.index = df.index
     df = pd.concat([df.drop(['OffensePersonnel'], axis=1), offense_personnel_df], axis=1)
+    return df
+
+def engineer_features(df):
+    df['DefendersInTheBox_vs_Distance'] = (df['DefendersInTheBox'] / df['Distance'])
+    return df
+
+
+def preprocess_features(df):
+    """Accepts df like train data, returns cleaned, standartized and enriched df"""
+
+    df = clean_abbrs(df)
+
+    df = standartize_orientations(df)
+
+    df = clean_turf(df)
+
+
+    df['StadiumType'] = df['StadiumType'].apply(clean_StadiumType)
+    df['StadiumTypeShort'] = df['StadiumType'].apply(transform_StadiumType)
+    df = df.drop(['StadiumType'], axis=1)
+
+    df['HomeField'] = df['FieldPosition'] == df['HomeTeamAbbr']
+    df['Field_eq_Possession'] = df['FieldPosition'] == df['PossessionTeam']
+    df['HomePossesion'] = (df['PossessionTeam'] == df['HomeTeamAbbr'])
+
+    df = encode_formations(df)
+
+    df['GameClock'] = df['GameClock'].apply(str_to_seconds)
+
+    df['PlayerHeight'] = df['PlayerHeight'].apply(lambda x: 12*int(x.split('-')[0])+int(x.split('-')[1]))
+    df['PlayerBMI'] = 703*(df['PlayerWeight']/(df['PlayerHeight'])**2)
+
+    df['TimeHandoff'] = pd.to_datetime(df['TimeHandoff'], utc=True)
+    df['TimeSnap'] = pd.to_datetime(df['TimeSnap'], utc=True)
+    df['TimeDelta'] = (df['TimeHandoff']-df['TimeSnap']).apply(lambda x: x.total_seconds())
+    df['PlayerBirthDate'] = df['PlayerBirthDate'].apply(lambda x: datetime.datetime.strptime(x, "%m/%d/%Y"))
+    df['PlayerBirthDate'] = pd.to_datetime(df['PlayerBirthDate'], utc=True)
+
+    seconds_in_year = 60*60*24*365.25
+    df['PlayerAge'] = (df['TimeHandoff']-df['PlayerBirthDate']).apply(lambda x: x.total_seconds())/seconds_in_year
+    # df = df.drop([], axis=1)
+
+    df = clean_weather(df)
+
+    df['IsRusher'] = df['NflId'] == df['NflIdRusher']
     
 
-    df = sort_df(df)
+    df = encode_personell(df)
 
-    if fillna:
-        df.fillna(-999, inplace=True)
+    # Feature engineering
+    df = engineer_features(df)
+
+    df = sort_df(df)
+    
     return df
 
 def sort_df(df):
-    df = df.sort_values(by=['PlayId', 'InPosession', 'IsRusher']).reset_index(drop=True)
+    df.sort_values(by=['PlayId', 'IsOnOffense', 'IsRusher'], inplace=True)
     return df
 
-def make_x(df):
+def make_x(df, fillna=True):
     source_play_id = df['PlayId']
 
-    cols_delete = ['GameId', 'PlayId', 'IsRusher', 'Team']
-    df = df.drop(cols_delete, axis=1)
+    df = final_drop_cols(df)
 
-    # Fill nan
-
-    # Text features
-    text_cols = [
-        'FieldPosition',
-         'DisplayName',
-         'PossessionTeam',
-         'PlayerCollegeName',
-         'Position',
-         'HomeTeamAbbr',
-         'VisitorTeamAbbr',
-         'Stadium',
-         'Location']
-    df = df.drop(text_cols, axis=1)
+    if fillna:
+        df.fillna(-999, inplace=True)
 
     # Player features
     cols_player = ['X',
@@ -290,6 +350,7 @@ def make_x(df):
          'PlayerAge']
 
     all_cols_player = np.array([[f'pl{num}_'+x for x in cols_player] for num in range(1, 23)]).flatten()
+    # print('cols player', all_cols_player)
 
     X = np.array(df[cols_player]).reshape(-1, len(cols_player)*22)
 
@@ -300,7 +361,9 @@ def make_x(df):
     assert df[cols_player].shape[1] == X_df.shape[1] / 22
 
     # Play features
-    cols_play = list(df.drop(cols_player+(['Yards'] if 'Yards' in df.columns else []), axis=1).columns)
+    # print(list(df.drop(cols_player, axis=1).columns))
+    cols_play = ['Season', 'YardLine', 'Quarter', 'GameClock', 'Down', 'Distance', 'HomeScoreBeforePlay', 'VisitorScoreBeforePlay', 'DefendersInTheBox', 'Week', 'GameWeather', 'Temperature', 'Humidity', 'WindSpeed', 'ToLeft', 'IsOnOffense', 'HomeOnOffense', 'TurfIsNatural', 'StadiumTypeShort', 'HomeField', 'Field_eq_Possession', 'HomePossesion', 'OffenseFormation_ACE', 'OffenseFormation_EMPTY', 'OffenseFormation_I_FORM', 'OffenseFormation_JUMBO', 'OffenseFormation_PISTOL', 'OffenseFormation_SHOTGUN', 'OffenseFormation_SINGLEBACK', 'OffenseFormation_WILDCAT', 'TimeDelta', 'defense_DB', 'defense_DL', 'defense_LB', 'defense_OL', 'defense_QB', 'defense_RB', 'defense_TE', 'defense_WR', 'offense_DB', 'offense_DL', 'offense_LB', 'offense_OL', 'offense_QB', 'offense_RB', 'offense_TE', 'offense_WR', 'DefendersInTheBox_vs_Distance']
+
     X_play_col = np.zeros(shape=(X.shape[0], len(cols_play)))
     for i, col in enumerate(cols_play):
         X_play_col[:, i] = df[col][::22]
